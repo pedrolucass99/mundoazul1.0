72e080216eaee30f6c4ff2219b7364c9ffdf6125
pedrolucass99 <pedrorochi73@gmail.com>
2018-12-14 16:49:11 -0300

    continucao da edicao

--- resources/views/ola.blade.php
+++ resources/views/ola.blade.php
@@ -12,8 +12,8 @@
        <p>Veja os eventos que acontecerão</p>
      </header>
 		<div class="row about-cols">
-					@foreach($evento as $evento)
-				<div class="col-md-4 wow fadeInUp">
+					@foreach($evento as $key=>$evento)
+				<div class="col-md-4 wow fadeInUp" data-wow-delay="0.{{$key}}s">
           <div class="about-col">
   				  <div class="img">
                 <img src="img/about-mission.jpg" alt="" class="img-fluid">
@@ -32,5 +32,24 @@
 		</div>
   </div>
 </section>
+<script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/waypoints/waypoints.min.js"></script>
+  <script src="lib/counterup/counterup.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/isotope/isotope.pkgd.min.js"></script>
+  <script src="lib/lightbox/js/lightbox.min.js"></script>
+  <script src="lib/touchSwipe/jquery.touchSwipe.min.js"></script>
+  <!-- Contact Form JavaScript File -->
+  <script src="contactform/contactform.js"></script>
+
+  <!-- Template Main Javascript File -->
+  <script src="js/main.js"></script>
+
 @endsection
 
