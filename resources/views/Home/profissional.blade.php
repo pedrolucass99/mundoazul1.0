0af8e7538b9b02e8262b669db188c27bcb52200a
pedrolucass99 <pedrorochi73@gmail.com>
2018-12-18 17:49:17 -0300

    perfil

--- resources/views/Home/profissional.blade.php
+++ resources/views/Home/profissional.blade.php
@@ -6,7 +6,7 @@
 
 </style>
 </head>
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8" id="tudo">
             <div class="card">
@@ -42,7 +42,38 @@
 
                 </div>
             </div>
+        </div>   -->
+    <div class="container">
+<div class="main-content">
+    <div class="container">
+        <h3 class="site-title">{{ Auth::user()->name }}</h3>
+        <div class="row">
+            <div class="col-m-9">
+                <div class="row">
+                 <div class="panel">
+                <div class=" panel-body ">
+                   
+                    <div class="col-md-5">
+                        <img class="img-responsive" src="/images/{{$profissional->filename}}" >
+                        <i class="fa fa-wrench"> </i>
+            <div class=" clearfix">
+               
+                <h4>Profissional:{{ $profissional['especializacao'] }}</h4>
+
+               <hr>
+                    </div>
+                     </div>
+                    </div>
+                    </div>
+                </div>
+             </div>
         </div>
+    </div>
+</div>
+                <a href="{{action('ProfissionalController@show', $profissional['id_user'].'.ver')}}" class="btn btn-info">Ver Mensagem</a></td>
+                <a href="{{action('ProfissionalController@edit',  Auth::id())}}" class="btn btn-primary">Editar conta</a>
+                 
+                          </div>
 <!--                 @if(Auth::user()->tipo == 2)                               
                     <div class="card" id="teste">
                         <div class="card-header">Seu perfil profissional, {{ Auth::user()->name }}</div>
